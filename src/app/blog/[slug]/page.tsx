843cf5a9ed86b6e5b0c42d0d1fd0024f50e5af3b
VRIBΞS.DEV <vribeslopez@gmail.com>
2026-01-02 18:46:56 +0100

    fix: add missing Link import to blog post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { getPublishedPosts, getPostBySlug } from "@/app/actions/blog";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 import ReactMarkdown from "react-markdown";
 import { Metadata } from "next";
 
