00f837c2a0960c17e5f7c9fed095c47c9a0ccf6b
VRIBΞS.DEV <vribeslopez@gmail.com>
2026-01-13 17:23:28 +0100

    fix: remove temporary debug files to fix cloudflare build

--- debug_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Starting debug script" > debug_status.txt
-npm run dev > dev_log.txt 2>&1 &
-echo $! > dev_pid.txt
-echo "Server started with PID $(cat dev_pid.txt)" >> debug_status.txt
